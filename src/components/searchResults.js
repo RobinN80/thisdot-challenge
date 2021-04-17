c870fe8e6fbd029ccedb35ef38c9d6329a82e4d6
Robin Novak <robin.m.novak@gmail.com>
2021-04-17 09:18:06 -0700

    filtered the list of users and added catch method to get request

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
-import axios from 'axios';
-import User from './user';
+import axios from "axios";
+import User from "./user";
 
 class SearchResults extends Component {
   constructor(props) {
@@ -8,6 +8,7 @@ class SearchResults extends Component {
     this.state = {
       value: "Search GitHub Users",
       users: [],
+      loading: false,
     };
   }
 
@@ -16,19 +17,34 @@ class SearchResults extends Component {
   };
 
   handleSubmit = (event) => {
-    event.preventDefault();
-
-    axios.get(`https://api.github.com/users/${this.state.value}`)
-    .then(res => {
+    
+    // borrowed code below from www.pluralsight.com/guides/axios-vs-fetch
+    axios
+      .get('https://api.github.com/users')
+      .then((res) => {
         console.log("handleSubmit response", res.data);
-        this.setState({users: [res.data]});
-    });
+        this.setState({ users: res.data });
+      })
+      .catch(
+        error => {
+            if (error.response) {
+              console.log(error.response.data)
+            } else if (error.request) {
+              console.log(error.request)
+            } else {
+              console.log("Error", error.message)
+            }
+            console.log(error.config)
+          }
+      );
 
     console.log("Current state is:" + JSON.stringify(this.state));
     // alert("Current state is:" + JSON.stringify(this.state));
+    event.preventDefault();
   };
 
   render() {
+    const { value, users } = this.state;
     return (
       <div>
         {/* borrowed code for form from reactjs.org/docs/forms.html */}
@@ -36,8 +52,9 @@ class SearchResults extends Component {
           <label>
             Search:{" "}
             <input
-              type="text"
-              placeholder={this.state.value}
+              type="search"
+              name="usersearch"
+              placeholder={value}
               onChange={this.handleChange}
             />
           </label>
@@ -45,9 +62,20 @@ class SearchResults extends Component {
         </form>
         <h2>Results:</h2>
         <div>
-          {this.state.users.map((USER) => {
-            return <User key={USER.id} user={USER} />;
-          })}
+          {/* borrowed code below From PedroTech youtube channel "Search Filter React Tutorial" */}
+          {users
+            .filter((USER) => {
+              if (value == "") {
+                return;
+              } else if (
+                USER.login.toLowerCase().includes(value.toLowerCase())
+              ) {
+                return USER.login;
+              }
+            })
+            .map((USER) => {
+              return <User key={USER.id} user={USER} />;
+            })}
         </div>
       </div>
     );
