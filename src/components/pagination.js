b458355b6b2c68a6f105132142159a9337e25965
Robin Novak <robin.m.novak@gmail.com>
2021-04-18 08:39:15 -0700

    updated pagination navbar

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,7 +1,7 @@
 import React from "react";
 import lodash from "lodash";
 
-//borrowed code from Code with Mosh React Tutorial
+//borrowed some code from Code with Mosh React Tutorial with the exception of the li prev and next.
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
   //console.log(currentPage);
@@ -14,11 +14,22 @@ const Pagination = (props) => {
   return (
     <nav>
       <ul className="pagination">
+        <li className="page-item" onClick={() => { if (currentPage > 1) onPageChange(currentPage - 1)}}>
+          Prev
+        </li>
         {pages.map((page) => (
-          <li key={page} className={ page === currentPage ? 'page-item active' : 'page-item'}>
-            <a className="page-link" onClick= {() => onPageChange(page)}>{page}</a>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
           </li>
         ))}
+        <li className="page-item" onClick={() => {if(pagesCount > currentPage) onPageChange(currentPage + 1)}}>
+          Next
+        </li>
       </ul>
     </nav>
   );
