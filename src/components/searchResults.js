bff5a092db0a15d6c50d9894aba6deab389f33f8
Robin Novak <robin.m.novak@gmail.com>
2021-04-18 17:56:40 -0700

    added componentdidupdate *handleSubmit not working

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -8,14 +8,16 @@ class SearchResults extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "robin",
+      value: "q",
       users: [],
-      loading: false,
       pageSize: 5,
       currentPage: 1,
       total_count: 0,
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   componentDidMount() {
     // borrowed code below from www.pluralsight.com/guides/axios-vs-fetch
     axios
@@ -39,14 +41,39 @@ class SearchResults extends Component {
       });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log("PrevState", prevState);
+    if (prevState.value !== this.state.value) {
+      axios
+        .get(`https://api.github.com/search/users?q=${this.state.value}`)
+        .then((res) => {
+          //console.log("get response", res.data);
+          this.setState({
+            users: res.data.items,
+            total_count: res.data.total_count,
+          });
+        })
+        .catch((error) => {
+          if (error.response) {
+            console.log(error.response.data);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+    }
+  }
+
   handleSubmit = (event) => {
-    console.log("Current State is:", +JSON.stringify(this.state));
+    console.log("Current State is:", JSON.stringify(this.state));
     this.setState({ value: event.target.value });
   };
 
-//   handlePageChange = (event) => {
-//     this.setState({ value: event.target.value });
-//   };
+    // handleChange = (event) => {
+    //   this.setState({ value: event.target.value });
+    // };
 
   handlePageChange = (page) => {
     this.setState({ currentPage: page });
@@ -54,14 +81,6 @@ class SearchResults extends Component {
 
   render() {
     const { value, users, pageSize, currentPage, total_count } = this.state;
-
-    // const usersFiltered = users.filter((USER) => {
-    //   if (value === "") {
-    //     return [];
-    //   } else if (USER.login.toLowerCase().includes(value.toLowerCase())) {
-    //     return USER.login;
-    //   }
-    // });
 
     const usersPaginated = paginate(users, currentPage, pageSize);
 
@@ -75,10 +94,10 @@ class SearchResults extends Component {
               type="search"
               name="usersearch"
               placeholder="Search GitHub Users"
-              //onChange={this.handleChange}
+              onChange={this.handleChange}
             />
           </label>
-          <input type="submit" value="Search" />
+          <input type="submit" value="Search" onSubmit={this.handleSubmit} />
         </form>
         <h2>{total_count} results</h2>
         <table>
